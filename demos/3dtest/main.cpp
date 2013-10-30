d38675ab07a6a42555289bc14ccf7fa80f181df9
Pierre Bourdon <delroth@gmail.com>
2013-10-31 00:39:50 +0100

    Change the demo triangle to a cube and add a flashing background when pressing A

--- demos/3dtest/main.cpp
+++ demos/3dtest/main.cpp
@@ -21,7 +21,7 @@ void InitRendering() {
 }
 
 void RenderFrame(const drc::InputData& input_data) {
-  static float rot[2], trans[2];
+  static float rot[2], trans[2], bkgd;
 
   if (input_data.valid) {
     trans[0] += input_data.left_stick_x / 40.0;
@@ -29,9 +29,13 @@ void RenderFrame(const drc::InputData& input_data) {
 
     rot[0] += input_data.right_stick_x * 2.0;
     rot[1] += input_data.right_stick_y * 2.0;
+
+    if (input_data.buttons & drc::InputData::kBtnA) {
+      bkgd = 1.0;
+    }
   }
 
-  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glClearColor(bkgd, bkgd, bkgd, 0.0);
   glClearDepth(1.0);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -39,14 +43,51 @@ void RenderFrame(const drc::InputData& input_data) {
   glTranslated(0.0, 0.0, -6.0);
 
   glTranslated(trans[0], trans[1], 0.0);
-  glRotated(rot[0], 1.0, 0.0, 0.0);
-  glRotated(rot[1], 0.0, 1.0, 0.0);
+  glRotated(rot[0], 0.0, 1.0, 0.0);
+  glRotated(rot[1], 1.0, 0.0, 0.0);
 
-  glBegin(GL_TRIANGLES);
-    glVertex3d(0.0, 1.0, 0.0);
-    glVertex3d(-1.0, -1.0, 0.0);
-    glVertex3d(1.0, -1.0, 0.0);
+  // Draw a cube (from NeHe's tutorials...).
+  glBegin(GL_QUADS);
+    glColor3f(0.0f,1.0f,0.0f);
+    glVertex3f( 1.0f, 1.0f,-1.0f);
+    glVertex3f(-1.0f, 1.0f,-1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f( 1.0f, 1.0f, 1.0f);
+
+    glColor3f(1.0f,0.5f,0.0f);
+    glVertex3f( 1.0f,-1.0f, 1.0f);
+    glVertex3f(-1.0f,-1.0f, 1.0f);
+    glVertex3f(-1.0f,-1.0f,-1.0f);
+    glVertex3f( 1.0f,-1.0f,-1.0f);
+
+    glColor3f(1.0f,0.0f,0.0f);
+    glVertex3f( 1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f,-1.0f, 1.0f);
+    glVertex3f( 1.0f,-1.0f, 1.0f);
+
+    glColor3f(1.0f,1.0f,0.0f);
+    glVertex3f( 1.0f,-1.0f,-1.0f);
+    glVertex3f(-1.0f,-1.0f,-1.0f);
+    glVertex3f(-1.0f, 1.0f,-1.0f);
+    glVertex3f( 1.0f, 1.0f,-1.0f);
+
+    glColor3f(0.0f,0.0f,1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f,-1.0f);
+    glVertex3f(-1.0f,-1.0f,-1.0f);
+    glVertex3f(-1.0f,-1.0f, 1.0f);
+
+    glColor3f(1.0f,0.0f,1.0f);
+    glVertex3f( 1.0f, 1.0f,-1.0f);
+    glVertex3f( 1.0f, 1.0f, 1.0f);
+    glVertex3f( 1.0f,-1.0f, 1.0f);
+    glVertex3f( 1.0f,-1.0f,-1.0f);
   glEnd();
+
+  if (bkgd > 0.0) {
+    bkgd -= 0.02;
+  }
 }
 
 }  // namespace
