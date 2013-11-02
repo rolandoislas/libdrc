d6b65520f4457be88a57182b2b851525c7fef5ff
Pierre Bourdon <delroth@gmail.com>
2013-11-02 04:33:13 +0100

    Add a stupid compile.sh - too lazy for cmake atm

---
+++ compile.sh
@@ -0,0 +1,1 @@
+g++ -Wall -Wextra -Wno-unused-parameter -Wno-unused-variable -Iinclude -std=gnu++11 demos/3dtest/main.cpp demos/framework/framework.cpp src/input.cpp src/streamer.cpp src/udp.cpp src/video-converter.cpp `pkg-config --cflags sdl glew glu gl libswscale` `pkg-config --libs sdl glew glu gl libswscale` -g -ggdb3
