8e11483ec659a07793ce1110f57f239605c9e8bd
Pierre Bourdon <delroth@gmail.com>
2013-11-03 03:13:05 +0100

    Implement stupid GL frame readback that flips the frame on CPU side

--- demos/framework/framework.cpp
+++ demos/framework/framework.cpp
@@ -88,6 +88,21 @@ std::vector<drc::u8> TryReadbackFromGL() {
 
   // TODO: PBO
 
+  // Very stupid implementation - ideally the rotating should be done on GPU
+  // side, and the GPU should directly output YUV data.
+  ret.resize(drc::kScreenWidth * drc::kScreenHeight * 4);
+  glReadPixels(0, 0, drc::kScreenWidth, drc::kScreenHeight, GL_BGRA,
+               GL_UNSIGNED_BYTE, ret.data());
+  for (int y = 0; y <= drc::kScreenHeight / 2; ++y) {
+    int base = drc::kScreenWidth * y;
+    int rev_base = drc::kScreenWidth * (drc::kScreenHeight - y - 1);
+    for (int x = 0; x < drc::kScreenWidth * 4; ++x) {
+      int tmp = ret[rev_base + x];
+      ret[rev_base + x] = ret[base + x];
+      ret[base + x] = tmp;
+    }
+  }
+
   return ret;
 }
 
