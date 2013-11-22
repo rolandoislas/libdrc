87d1cf75a67f49ebd045882bbb34478323413f06
Pierre Bourdon <delroth@gmail.com>
2013-11-22 08:47:59 +0100

    Change encoder quality preset from slow to fast and remove x264 printfs

--- src/h264-encoder.cpp
+++ src/h264-encoder.cpp
@@ -16,11 +16,10 @@ extern "C" {
 namespace drc {
 
 namespace {
-const char* const kEncoderQuality = "slow";
+const char* const kEncoderQuality = "fast";
 
 void log_encoder_messages(void* data, int i_level, const char* format,
                           va_list va) {
-  vfprintf(stderr, format, va);
 }
 }  // namespace
 
